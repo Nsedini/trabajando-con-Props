55b32f87e44e7d541131e7fc5ae79ba99a60fc1b
Natalio Sedini BAscur <69011412+Nsedini@users.noreply.github.com>
2021-09-23 18:32:07 -0300

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Todo :title="'Lista del supermercado'" />
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue'
+export default {
+  name: 'App',
+  components: {
+    Todo,
+  },
+}
+</script>
